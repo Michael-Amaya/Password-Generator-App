4d9a393e73fce75be02556bf7276871d0ef61f3a
Michael Amaya <mikeamaya8@gmail.com>
2023-01-12 17:41:01 -0500

    Fix file structure and add forms for the login page and the registration page

--- webapp/app.py
+++ webapp/app.py
@@ -1,12 +1,22 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
+@app.route("/users/login")
+def login_page():
+    return render_template('login.html')
+
+
+@app.route("/users/register")
+def registration_page():
+    return render_template('register.html')
+
+
 @app.route("/")
 def app_main():
-    return "Hello world!"
+    return 'Hello world!'
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(port=5001)
