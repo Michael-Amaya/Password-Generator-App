42df937a028ee8d784b7c63f525de8adda89b99f
Michael Amaya <mikeamaya8@gmail.com>
2023-01-14 21:06:39 -0500

    Add logic to register, login, and logout users.

--- webapp/login_register.py
+++ webapp/login_register.py
@@ -1,36 +1,137 @@
-import secrets
 import json
-from flask import Blueprint, render_template, request, url_for, redirect
+from flask import Blueprint, render_template, request, url_for, redirect, \
+    session
+from argon2 import PasswordHasher
+from argon2.exceptions import VerifyMismatchError
+from database.database_users import create_user, get_user_data, \
+    change_user_password
 from settings import SETTINGS
-
 
 
 login_register = Blueprint('login_register', __name__,
                            template_folder='templates')
 
+# Login error message to stay consistent
+login_error_message = 'There was an issue logging in!'
+
 
 @login_register.route('/users/login_main', methods=['POST'])
 def perform_login():
-    pass
+    user_data = request.form
+    user_record = get_user_data(user_data['email'])
+    if user_record is None:
+        messages = {
+            'message': login_error_message,
+            'heading': 'Error',
+            'type': 'danger'
+        }
+
+        return redirect(url_for('login_register.login_page',
+                        messages=json.dumps(messages)))
+
+    peppered_password = f"{user_data['password']}{SETTINGS['password_pepper']}"
+    password_hasher = PasswordHasher()
+
+    try:
+        password_hasher.verify(user_record['password'], peppered_password)
+        # Throws an exception if the password doesn't verify, so by this point,
+        # we should be verified
+
+        if password_hasher.check_needs_rehash(user_record['password']):
+            new_hashed_password = password_hasher.hash(user_data['password'])
+            change_user_password(user_record['email'], new_hashed_password)
+
+        # According to flask, session just exists? It's to track the user
+        # session
+        session['email'] = user_record['email']
+        session['name'] = user_record['name']
+        session['birthday'] = user_record['birthday']
+
+        messages = {
+            'message': 'You have successfully logged in!',
+            'heading': 'Success',
+            'type': 'success'
+        }
+
+        return redirect(url_for('app_main', messages=json.dumps(messages)))
+    except VerifyMismatchError:
+        messages = {
+            'message': login_error_message,
+            'heading': 'Error',
+            'type': 'danger'
+        }
+
+        return redirect(url_for('login_register.login_page',
+                        messages=json.dumps(messages)))
 
 
 @login_register.route('/users/register_main', methods=['POST'])
 def perform_register():
     user_data = request.form
-    
-    # messages = json.dumps({'message': 'test', 'heading': 'Testing!',
-    #                       'type': 'success'})
-    # return render_template('index.html', message='test',
-    #                        message_header='Testing!',
-    #                        message_type='success')
-    # return redirect(url_for('app_main', messages=messages))
+    if user_data['password'] != user_data['password_again']:
+        message = {
+            'message': 'The password do not match!',
+            'heading': 'Error',
+            'type': 'danger'
+        }
+
+        return redirect(url_for('login_register.registration_page',
+                        messages=json.dumps(message)))
+
+    # Hash password
+    peppered_password = f"{user_data['password']}{SETTINGS['password_pepper']}"
+    peppered_pin = f"{user_data['pin']}{SETTINGS['pin_pepper']}"
+
+    password_hasher = PasswordHasher()
+    password_hashed = password_hasher.hash(peppered_password)
+    pin_hashed = password_hasher.hash(peppered_pin)
+
+    create_user(user_data['name'], user_data['birthday'], user_data['email'],
+                password_hashed, pin_hashed)
+
+    message = {
+        'message': 'You have been registered! Please login to continue.',
+        'heading': 'Success',
+        'type': 'success'
+    }
+
+    return redirect(url_for('app_main', messages=json.dumps(message)))
 
 
-@login_register.route("/users/login")
+@login_register.route('/users/logout')
+def perform_logout():
+    if session.get('email'):
+        session.pop('email')
+        session.pop('name')
+        session.pop('birthday')
+        messages = {
+            'message': 'You have been logged out!',
+            'heading': 'Success',
+            'type': 'success'
+        }
+
+        return redirect(url_for('app_main', messages=json.dumps(messages)))
+    else:
+        messages = {
+            'message': 'You are not logged in!',
+            'heading': 'Error',
+            'type': 'danger'
+        }
+
+        return redirect(url_for('app_main', messages=json.dumps(messages)))
+
+
+@login_register.route('/users/login')
 def login_page():
-    return render_template('login.html')
+    messages = request.args.get('messages', None)
+    if messages:
+        messages = json.loads(messages)
+    return render_template('login.html', messages=messages)
 
 
-@login_register.route("/users/register")
+@login_register.route('/users/register')
 def registration_page():
-    return render_template('register.html')
+    messages = request.args.get('messages', None)
+    if messages:
+        messages = json.loads(messages)
+    return render_template('register.html', messages=messages)
