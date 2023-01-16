0c96a32247d233f79ad582e5abe9be2d4512b772
Michael Amaya <mikeamaya8@gmail.com>
2023-01-16 17:53:08 -0500

    Add login required decorator so checks don't need to be made on every page

---
+++ webapp/utils.py
@@ -0,0 +1,20 @@
+import json
+from functools import wraps
+from flask import session, url_for, redirect
+
+
+def login_required(status=None):
+    def login_decorator(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            if 'email' in session and (status is None or status in session):
+                return func(*args, **kwargs)
+            else:
+                messages = {
+                    'message': 'You must be logged in to access that page!',
+                    'heading': 'Error',
+                    'type': 'danger'
+                }
+
+                return redirect(url_for('login_register.login_page',
+                                        messages=json.dumps(messages)))
