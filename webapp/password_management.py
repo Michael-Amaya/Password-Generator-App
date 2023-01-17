3ec44214d241c6087492db965ab98eebf2f2dddb
Michael Amaya <mikeamaya8@gmail.com>
2023-01-16 20:01:07 -0500

    Add logic for viewing passwords and basis for creating passwords

--- webapp/password_management.py
+++ webapp/password_management.py
@@ -1,11 +1,37 @@
-from flask import Blueprint, render_template
-from utils import login_required
+import json
+from flask import Blueprint, render_template, session, redirect, url_for
+from database.database_passwords import get_user_passwords
+from utils import login_required, UserDifferenceException
 
 password_management = Blueprint('password_management', __name__,
                                 template_folder='templates')
 
 
-@password_management.route('/passwords')
+@password_management.route('/')
 @login_required()
 def password_main():
     return render_template('passwords_main.html')
+
+
+@password_management.route('/view_passwords')
+@login_required()
+def view_passwords_main():
+    try:
+        user_passwords = get_user_passwords(session['email'])
+        return render_template('view_passwords.html',
+                               user_passwords=user_passwords)
+    except UserDifferenceException:
+        messages = {
+            'message': 'There was an error getting your information.',
+            'heading': 'Error',
+            'type': 'danger'
+        }
+
+        return redirect(url_for('password_management.password_main',
+                                messages=json.dumps(messages)))
+
+
+@password_management.route('/create_password')
+@login_required()
+def create_password_main():
+    pass
