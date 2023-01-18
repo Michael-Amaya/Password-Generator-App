18712015a8bd12a5dfc7d9f556a09652996cce11
Michael Amaya <mikeamaya8@gmail.com>
2023-01-17 22:33:32 -0500

    Add logic to view individual password

--- webapp/password_management.py
+++ webapp/password_management.py
@@ -5,7 +5,7 @@ from flask import Blueprint, render_template, session, redirect, url_for, \
     request
 from argon2 import PasswordHasher
 from database.database_passwords import get_user_passwords, \
-    insert_user_password
+    insert_user_password, get_individual_user_password
 from generate_password import generate_password
 from utils import login_required, UserDifferenceException, message_capable
 
@@ -26,7 +26,6 @@ def password_main(**kwargs):
 def view_passwords_main():
     try:
         user_passwords = get_user_passwords(session['email'])
-        # print(user_passwords[2]['password'].tobytes())
         return render_template('view_passwords.html',
                                user_passwords=user_passwords)
     except UserDifferenceException:
@@ -90,3 +89,40 @@ def create_password_logic():
 
     return redirect(url_for('password_management.password_main',
                             messages=json.dumps(messages)))
+
+
+@password_management.route('/view_password/<password_id>')
+@login_required()
+@message_capable()
+def view_password(password_id, **kwargs):
+    if kwargs.get('messages'):
+        return render_template('view_password.html',
+                               messages=kwargs.get('messages'))
+    password_info = get_individual_user_password(session['user_id'],
+                                                 password_id)
+    if password_info is None:
+        messages = {
+            'message': 'Password not found!',
+            'heading': 'Error',
+            'type': 'danger'
+        }
+
+        return redirect(url_for('password_management.view_password',
+                                password_id=password_id,
+                                messages=json.dumps(messages)))
+    # Decrypt password
+    cipher = AES.new(password_info['key'], AES.MODE_EAX,
+                     nonce=password_info['nonce'])
+    plaintext = cipher.decrypt(password_info['password'])
+    try:
+        cipher.verify(password_info['tag'])
+        password_found = plaintext.decode()
+    except ValueError:
+        messages = {
+            'message': 'There was an error getting your password!',
+            'heading': 'Error',
+            'type': 'danger'
+        }
+        return redirect(url_for('password_management.view_password',
+                                messages=json.dumps(messages)))
+    return render_template('view_password.html', password_found=password_found)
